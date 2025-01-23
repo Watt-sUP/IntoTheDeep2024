f13f4de4b52e912a6a1a661db3fea7969a1c69dc
scama01 <andreiser16@gmail.com>
2025-01-23 13:45:22 +0200

    Fix Gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BasketAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BasketAuto.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
 import com.arcrobotics.ftclib.command.WaitUntilCommand;
 import com.pedropathing.localization.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -7,6 +10,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.autonomous.assets.AutonomousOpMode;
 import org.firstinspires.ftc.teamcode.commands.FollowPointCommand;
+import org.firstinspires.ftc.teamcode.subsystems.OuttakeSubsystem;
 import org.firstinspires.ftc.teamcode.util.FixedSequentialCommandGroup;
 
 @Disabled
@@ -23,8 +27,28 @@ public class BasketAuto extends AutonomousOpMode {
         schedule(
                 new FixedSequentialCommandGroup(
                         new WaitUntilCommand(this::opModeIsActive),
-                        
+
                         new FollowPointCommand(follower, BASKET_POSE, 1)
+                                .alongWith(
+                                        new SequentialCommandGroup(
+                                                new WaitCommand(250),
+                                                new InstantCommand(() -> {
+                                                    outtake.setSlidesState(OuttakeSubsystem.SlidesState.HIGH_BASKET);
+                                                    outtake.setArmState(OuttakeSubsystem.ArmState.OUT);
+                                                    outtake.setPivotState(OuttakeSubsystem.PivotState.OUT);
+                                                    new WaitCommand(1750);
+                                                })
+                                        )
+                                ),
+
+                        new InstantCommand(() -> outtake.setClawState(OuttakeSubsystem.ClawState.OPENED))
+                                .alongWith(new WaitCommand(250)),
+
+                        new InstantCommand(() -> {
+                            outtake.setArmState(OuttakeSubsystem.ArmState.IN);
+                            outtake.setPivotState(OuttakeSubsystem.PivotState.IN);
+                        }).alongWith(new WaitCommand(200)),
+                        new InstantCommand(() -> outtake.setSlidesState(OuttakeSubsystem.SlidesState.LOWERED))
                 )
         );
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -16,9 +16,8 @@ public class LConstants {
         OTOSConstants.angleUnit = AngleUnit.RADIANS;
 
         OTOSConstants.offset = new SparkFunOTOS.Pose2D(-6.8897637795275, 0, 0);
-        OTOSConstants.useAccelerometer = false;
 
-        OTOSConstants.linearScalar = .97433;
+        OTOSConstants.linearScalar = .9665;
         OTOSConstants.angularScalar = .994;
     }
 }
